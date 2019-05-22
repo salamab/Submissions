1879b1636ec2d753c8a42726605a720068ad8062
salam <salamauce@gmail.com>
2019-05-22 23:58:04 +0300

    step 4 done

---
+++ Exercises/Javascript-Basics-01/Step_4.js
@@ -0,0 +1,5 @@
+var name= prompt("what is ur name?")
+var surname= prompt("what is ur surname?")
+var city = prompt("what is the name of ur city?")
+alert("Nom: " + name +"\n" + "Prenom: " + surname + "\n" + "Ville: " + city);
+alert("Hello " + name);
